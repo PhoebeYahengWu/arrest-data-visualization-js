1f2a6e364dc04c3b3f604d41931495eb22012369
wuyaheng <wuyaheng2016@gmail.com>
2020-09-17 19:36:40 -0400

    fix map

--- script.js
+++ script.js
@@ -8,78 +8,21 @@ $(document).ready(function () {
 
   $('#arresttype-dropdown').on('change', handleChange);
 
-  // const url = 'https://data.cityofnewyork.us/resource/uip8-fykc.json';
-  // let obj = {};
-
-  // $.getJSON(url, function (data) {
-  //   if (data.length) {
-  //     console.log(data)
-  //     for (let i = 0; i < data.length; i++) {
-  //       var key = data[i].ofns_desc;
-  //       if (key) obj[key] = true;
-  //     }
-  //   }
-
-  //   $.each(obj, function (key, entry) {
-  //     dropdown.append($('<option></option>').attr('value', key).text(key.toLowerCase()));
-  //   })
-
-  //   // let strUser = $("#arresttype-dropdown :selected").val(); 
-  //   // console.log(strUser)
-
-
-  //   // const lat = data.reduce((t, r) => t + parseFloat(r.latitude), 0) / data.length;
-
-  //   // const lon = data.reduce((t, r) => t + parseFloat(r.longitude), 0) / data.length;
-
-  //   // MAP = initMap(lat, lon)
-
-
-  //   // var mymap = L.map('mapid').setView([51.505, -0.09], 13);
-  //   // var mymap = L.map('mapid').setView([lat, lon], 10);
-
-  //   // L.tileLayer(
-  //   //   "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-  //   //     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-  //   //     maxZoom: 18,
-  //   //     id: "mapbox/streets-v11",
-  //   //     tileSize: 512,
-  //   //     zoomOffset: -1,
-  //   //     accessToken: 'pk.eyJ1Ijoic3RhcmR1c3QxOTEiLCJhIjoiY2thNjMzZzdlMDNtdTJ6bWptaTFqa3Y2MSJ9.VIX2KRmemtC5qDAMyL9Jug',
-  //   //   }
-  //   // ).addTo(mymap);
-
-  //   // var marker = L.marker([51.5, -0.09]).addTo(mymap);
-
-  //   data.forEach((ele) =>
-  //     // L.marker([ele.latitude, ele.longitude]).addTo(mymap).bindPopup(ele.arrest_date.substring(0, 10)).openPopup()
-  //     L.marker([ele.latitude, ele.longitude]).addTo(MAP)
-  //   );
-
-
-  //   var colorArray = [
-  //     "#FFB399",
-  //     "#E6B3B3",
-  //     "#6680B3",
-  //     "#FF99E6",
-  //     "#1AB399",
-  //     "#4D8066"
-  //   ];
-
-
-
-  // })
 })
 
 async function fetchRecords() {
   const url = 'https://data.cityofnewyork.us/resource/uip8-fykc.json';
   try {
     const response = await $.get(url)
+
     data = response
     buildDropDown(data)
 
-    MAP_OBJ = initMap(data)
+    MAP_OBJ = initMap(data) 
+    console.log(MAP_OBJ)
+
     addMarkers(data)
+    console.log(MAP_OBJ)
   } catch (error) {
     console.warn(error.message)
   }
@@ -94,6 +37,7 @@ function buildDropDown(data) {
 
   Array
     .from(OPTIONS)
+    .filter(x => x)
     .sort()
     .forEach(val => dropdown.append(
       $("<option>").val(val).text(val.toLowerCase())
@@ -101,6 +45,7 @@ function buildDropDown(data) {
 }
 
 function initMap(data) {
+  console.log("HERE")
   const lat = data.reduce((t, r) => t + parseFloat(r.latitude), 0) / data.length;
 
   const lon = data.reduce((t, r) => t + parseFloat(r.longitude), 0) / data.length;
@@ -118,6 +63,7 @@ function initMap(data) {
     }
   ).addTo(mymap);
 
+
   return mymap
 }
 
@@ -126,7 +72,7 @@ function addMarkers(data) {
   pins = []
 
   data.forEach((ele) =>
-    // L.marker([ele.latitude, ele.longitude]).addTo(mymap).bindPopup(ele.arrest_date.substring(0, 10)).openPopup()
+    //  L.marker([ele.latitude, ele.longitude]).addTo(mymap).bindPopup(ele.arrest_date.substring(0, 10)).openPopup()
     {
       const marker = L.marker([ele.latitude, ele.longitude])
       pins.push(marker)
